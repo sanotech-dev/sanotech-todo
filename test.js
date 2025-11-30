2c750b0b6f307d365fe3d6e394039dbe45536a3e
Sajjad <amir3hoshang@gmail.com>
2025-12-01 01:21:59 +0330

    تمرین درس صفر - متغیر و تابع

---
+++ test.js
@@ -0,0 +1,14 @@
+
+let name ="امیر هوشنگ";
+const age = "41";
+
+function salamBegu(){
+  console.log("سلام"+ name +"جان!");
+  console.log( "تو لان" + age +"سالته وباید بزور هم که شده برنامه نویس پولساز بشی جونت دربیاد!!");
+
+}
+
+salamBegu();
+function hadafam(){
+  console.log("هدف من تا فروردین ۱۴۰۵: اولین پروژه ۱۰۰ میلیونی");
+}
